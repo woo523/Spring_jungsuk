fcca482d4f760fc2ac67f0ccdc08aba2e1ddc2f4
woo523 <124018409+woo523@users.noreply.github.com>
2023-11-18 16:30:31 +0900

    11/18

--- ch2/src/main/java/com/gugu/ch2/Ex.java
+++ ch2/src/main/java/com/gugu/ch2/Ex.java
@@ -2,14 +2,26 @@ package com.gugu.ch2;
 
 public class Ex {
 
+	static long startTime = 0;
 	public static void main(String[] args) {
 		ThreadEx1_1 t1 = new ThreadEx1_1();
-		
-		Runnable r = new ThreadEx1_2();
-		Thread t2 = new Thread(r);
-		
+		ThreadEx1_2 t2 = new ThreadEx1_2();
+				
 		t1.start();
 		t2.start();
+		
+		
+		startTime = System.currentTimeMillis();
+		
+//		try {
+//			t1.join(); // 쓰레드의 작업이 끝날때까지 기다린다
+//			t2.join();
+//		} catch (Exception e) {
+//		}
+		
+		System.out.println("소요시간"+(System.currentTimeMillis()-startTime));
+		
+		
 	}
 		
 }
@@ -17,18 +29,18 @@ public class Ex {
 
 class ThreadEx1_1 extends Thread{
 	public void run() {
-		for(int i=0; i<500; i++) {
-			System.out.print(this.getName());
+		for(int i=0; i<100; i++) {
+			System.out.print("-");
 		}
 	}
 	
 }
 
 
-class ThreadEx1_2 implements Runnable{
+class ThreadEx1_2 extends Thread{
 	public void run() {
-		for(int i=0; i<500; i++) {
-			System.out.print(Thread.currentThread().getName());
+		for(int i=0; i<100; i++) {
+			System.out.print("|");
 		}
 	}
 	
---
+++ ch2/src/main/java/com/gugu/ch2/Ex2.java
@@ -0,0 +1,21 @@
+package com.gugu.ch2;
+
+
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+		
+		for(int i =1;i<=9;i++) {
+			
+			for(int j = 10-i;j>0;j--) {
+				System.out.print("*");
+				
+			}
+			System.out.println();
+		}
+		
+		
+	}
+
+}
